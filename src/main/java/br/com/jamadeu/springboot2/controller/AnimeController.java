7383e607ed2481835b3a2f1ab8e3fb99fab63bca
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-25 14:24:10 -0300

    DeleteMapping

--- src/main/java/br/com/jamadeu/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/jamadeu/springboot2/controller/AnimeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +33,13 @@ public class AnimeController {
   }
 
   @PostMapping
-  public ResponseEntity<Anime> save(@RequestBody Anime anime){
+  public ResponseEntity<Anime> save(@RequestBody Anime anime) {
     return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Anime> delete(@PathVariable long id) {
+    animeService.delete(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
