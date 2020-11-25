6eeed0483627ad7b8117f7668aafe133f0159b56
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-25 15:48:49 -0300

    add spring data jpa

--- src/main/java/br/com/jamadeu/springboot2/domain/Anime.java
+++ src/main/java/br/com/jamadeu/springboot2/domain/Anime.java
@@ -1,11 +1,23 @@
 package br.com.jamadeu.springboot2.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "animes")
 public class Anime {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
 }
