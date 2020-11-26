2db1dfdc0d18b1ddcec9a95680abada3da4904e8
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-26 10:47:00 -0300

    Refact

--- src/main/java/br/com/jamadeu/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/jamadeu/springboot2/controller/AnimeController.java
@@ -1,62 +1,52 @@
 package br.com.jamadeu.springboot2.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.jamadeu.springboot2.domain.Anime;
 import br.com.jamadeu.springboot2.requests.NewAnimeRequest;
 import br.com.jamadeu.springboot2.requests.ReplaceAnimeRequest;
 import br.com.jamadeu.springboot2.service.AnimeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("animes")
 @RequiredArgsConstructor
 public class AnimeController {
-  private final AnimeService animeService;
+    private final AnimeService animeService;
 
-  @GetMapping
-  public ResponseEntity<List<Anime>> list() {
-    return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
-  }
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
 
-  @GetMapping(path = "/{id}")
-  public ResponseEntity<Anime> findById(@PathVariable long id) {
-    return new ResponseEntity<>(animeService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
-  }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return new ResponseEntity<>(animeService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
+    }
 
-  @GetMapping(path = "/find")
-  public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
-    return new ResponseEntity<>(animeService.findByName(name), HttpStatus.OK);
-  }
+    @GetMapping(path = "/find")
+    public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
+        return new ResponseEntity<>(animeService.findByName(name), HttpStatus.OK);
+    }
 
-  @PostMapping
-  public ResponseEntity<Anime> save(@RequestBody @Valid NewAnimeRequest newAnimeRequest) {
-    return new ResponseEntity<>(animeService.save(newAnimeRequest), HttpStatus.CREATED);
-  }
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody @Valid NewAnimeRequest newAnimeRequest) {
+        return new ResponseEntity<>(animeService.save(newAnimeRequest), HttpStatus.CREATED);
+    }
 
-  @DeleteMapping(path = "/{id}")
-  public ResponseEntity<Anime> delete(@PathVariable long id) {
-    animeService.delete(id);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-  }
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Anime> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
-  @PutMapping
-  public ResponseEntity<Anime> replace(@RequestBody @Valid ReplaceAnimeRequest replaceAnimeRequest) {
-    animeService.replace(replaceAnimeRequest);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-  }
+    @PutMapping
+    public ResponseEntity<Anime> replace(@RequestBody @Valid ReplaceAnimeRequest replaceAnimeRequest) {
+        animeService.replace(replaceAnimeRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
