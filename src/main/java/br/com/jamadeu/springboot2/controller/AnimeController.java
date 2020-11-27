273c1357e8b093024bbeb4085041d2f25f165006
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-27 14:15:10 -0300

    Authentication Principal

--- src/main/java/br/com/jamadeu/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/jamadeu/springboot2/controller/AnimeController.java
@@ -5,11 +5,14 @@ import br.com.jamadeu.springboot2.requests.NewAnimeRequest;
 import br.com.jamadeu.springboot2.requests.ReplaceAnimeRequest;
 import br.com.jamadeu.springboot2.service.AnimeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("animes")
 @RequiredArgsConstructor
+@Log4j2
 public class AnimeController {
     private final AnimeService animeService;
 
@@ -33,6 +37,13 @@ public class AnimeController {
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return new ResponseEntity<>(animeService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "by-id/{id}")
+    public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable long id,
+                                                                 @AuthenticationPrincipal UserDetails userDetails) {
+        log.info(userDetails);
         return new ResponseEntity<>(animeService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
     }
 
